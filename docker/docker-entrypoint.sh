948745578e73673e9a7d9566f6addebb524e930b
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-09-23 12:58:29 -0700

    Remove commented out code

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -15,15 +15,9 @@ GID=$(id -g)
 # generate ssh keys
 gosu 0:0 ssh-keygen -A 2>/dev/null
 
-# update user and group ids
-#gosu 0:0 groupmod -g $GID ops 2>/dev/null
-#gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
-#gosu 0:0 usermod -aG docker ops 2>/dev/null
-
-# update ownership
-#gosu 0:0 chown -R $UID:$GID $HOME 2>/dev/null || true
-#gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
-#gosu 0:0 chown -R $UID:$GID /var/log/supervisor 2>/dev/null || true
+if [ -e /var/run/docker.sock ]; then
+  gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+fi
 
 # source bash profile
 source $HOME/.bash_profile
